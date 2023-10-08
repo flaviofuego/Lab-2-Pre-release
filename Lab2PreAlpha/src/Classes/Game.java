2421cccd3c381822f50dea0793833c0634c99e41
flaviofuego <flavioarregoces@gmail.com>
2023-10-08 13:11:49 -0500

    structure 1.1

--- Lab2PreAlpha/src/Classes/Game.java
+++ Lab2PreAlpha/src/Classes/Game.java
@@ -1,4 +1,33 @@
 package Classes;
 
 public class Game {
+    Campo campo;
+    int momento;
+
+    public Game(Campo campo){
+        this.campo = campo;
+        this.momento = 0;
+    }
+
+    public void mostrargrafo(){
+        System.out.println("Matriz de adyacencia:");
+        for (int i = 0; i < campo.jugadores.size(); i++) {
+            System.out.print(campo.jugadores.get(i).nombre + ",");
+            for (int j = 0; j < campo.jugadores.size(); j++) {
+                System.out.print(campo.matrizadyacencia[i][j] + ",");
+            }
+            System.out.println("");
+        }
+    }
+    public void mostrarrutas(){
+        System.out.println("Rutas:");
+        for (int i = 0; i < campo.rutas.size(); i++) {
+            System.out.print(campo.rutas.get(i) + ",");
+        }
+        System.out.println("");
+    }
+    public void cambiarmomento(int momento){
+        this.momento = momento;
+    }
+
 }
