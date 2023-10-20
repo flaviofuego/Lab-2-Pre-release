c536220e27f20668ae14c0931c39f0917490e50b
flaviofuego <flavioarregoces@gmail.com>
2023-10-20 01:57:24 -0500

    Lab 100%

--- Lab2PreAlpha/src/Frames/ver.java
+++ Lab2PreAlpha/src/Frames/ver.java
@@ -20,7 +20,7 @@ public class ver extends javax.swing.JPanel {
     public void setCampo(Campo campo) {
         this.campo = campo;
     }
-    public static int poto=0;
+
     protected void dibuja_camino(Graphics g, ArrayList<Integer> lista) {
         ArrayList<Arista> aristas = new ArrayList();
         for (int i = 0; i < lista.size(); i++) {
@@ -37,55 +37,25 @@ public class ver extends javax.swing.JPanel {
             Jugador nodo1 = arista.getJugador1();
             Jugador nodo2 = arista.getJugador2();
 
-            int x = nodo1.x;
-            int y = nodo1.y;
-            int w = nodo2.x;
-            int z = nodo2.y;
+            // g.drawLine(nodo1.x, nodo1.y, nodo2.x/2+ 50, nodo2.y/2 + 50);
+            // g.drawLine(nodo2.x/2+ 50, nodo2.y/2 + 50, nodo2.x/2+ 50, nodo2.y/2 + 50);
+            g.drawLine(nodo1.x, nodo1.y, nodo2.x, nodo2.y);
 
-            int x1 = x;
-            int y1 = y;
-            int e = 0, f = 0;
-            g.setColor(Color.BLACK);
-            if (x == w && y == z) {
-                g.drawArc(x + 10, y + 5, 20, 30, 320, 290);
-                x = x + 15;
-                y = y + 20;
-            } else {
-                e = x;
-                f = y;
-
-                // comprobar que 2 vertices no posean una arista
-                for (int i = 0; i < campo.getAristas().size(); i++) {
-                    if (campo.getAristas().get(i).getJugador1().x == w && campo.getAristas().get(i).getJugador1().y == z
-                            && campo.getAristas().get(i).getJugador2().x == x
-                            && campo.getAristas().get(i).getJugador2().y == y) {
-                        e = 1;
-                    }
-
-                }
-                if (e == 1) {
-                    g.drawLine(x, y, ((x - w) / 2) + w, ((y - z) / 2) + z - 10);
-                    g.drawLine(((x + 20 - w) / 2) + w, ((y + 45 - z) / 2) + z - 10, w, z);
-                    int x2 = ((x + 20 - w) / 2) + w;
-                    int y2 = ((y + 45 - z + 45) / 2) + z - 10;
-                    g.setColor(Color.decode("#C256FC"));
-                    System.out.println("Entro al 1");
-                    g.drawString(String.valueOf(arista.getPeso()), x2, y2 - 40);
-                } else {
-
-                    g.drawLine(x, y, ((x - w) / 2) + w, ((y - z) / 2) + z + 30);
-                    g.drawLine(((x - w) / 2) + w, ((y - z) / 2) + z + 30, w, z);
-                    int x2 = ((x + 20 - w) / 2) + w;
-                    int y2 = ((y + 45 - z + 45) / 2) + z + 30;
-                    g.setColor(Color.decode("#C256FC"));
-                    System.out.println("Entro al 2");
-                    g.drawString(String.valueOf(arista.getPeso()), x2, y2 + 30);
-                }
-            }
-
+            // Dibuja el valor de la arista
+            g.setColor(Color.RED);
+            int valorX = (nodo1.x + nodo2.x) / 2;
+            int valorY = (nodo1.y + nodo2.y) / 2;
+            g.drawString(String.valueOf(arista.getPeso()), valorX, valorY);
         }
     }
-
+    public Arista buscarArista(int x1, int y1, int x2, int y2){
+            for(Arista arista : campo.getAristas()){
+                if(arista.getJugador1().x == x1 && arista.getJugador1().y == y1 && arista.getJugador2().x == x2 && arista.getJugador2().y == y2){
+                    return arista;
+                }
+            }
+            return null;
+        }
     protected void dibuja(Graphics g) {
         super.paintComponent(g);
         int[] posisiones = { 1, 4, 4, 3 };
@@ -112,6 +82,28 @@ public class ver extends javax.swing.JPanel {
              * cantidad = 1;
              * }
              */
+            //Jugador1
+                /*X:10 Y:300
+                Jugador2
+                X:100 Y:150
+                Jugador3
+                X:122 Y:250
+                Jugador4
+                X:122 Y:350
+                Jugador5
+                X:100 Y:450
+                Jugador6
+                X:350 Y:200
+                Jugador7
+                X:350 Y:300
+                Jugador8
+                X:350 Y:400
+                Jugador9
+                X:600 Y:175
+                Jugador10
+                X:600 Y:300
+                Jugador11
+                X:600 Y:425*/
              if(iterador == 1){
                  nodo.x = 60;
                  nodo.y = 300;
@@ -119,10 +111,10 @@ public class ver extends javax.swing.JPanel {
                     nodo.x = 300;
                     nodo.y = 150;
                 }else if(iterador == 3){
-                    nodo.x = 500;
+                    nodo.x = 450;
                     nodo.y = 250;
                 }else if(iterador == 4){
-                    nodo.x = 500;
+                    nodo.x = 450;
                     nodo.y = 350;
                 }else if(iterador == 5){
                     nodo.x = 300;
@@ -149,29 +141,101 @@ public class ver extends javax.swing.JPanel {
              iterador++;
             System.out.println(nodo.x + ", " + nodo.y);
         }
-        // Dibuja las aristas y sus valores
+        //crea una funcion buscar arista por coordenadas
+        
+
+        ArrayList<Arista> aristaauxiliar = new ArrayList();
+
         for (Arista arista : campo.getAristas()) {
+            g.setColor(Color.BLACK);
+            Jugador nodo1 = arista.getJugador1();
+            Jugador nodo2 = arista.getJugador2();
+            int x = nodo1.x;
+            int y = nodo1.y;
+            int w = nodo2.x;
+            int z = nodo2.y;
+
+            //comprobar que la arista no esta en la lista auxiliar
+            if (!aristaauxiliar.contains(buscarArista(w, z, x, y))) {
+                aristaauxiliar.add(buscarArista(x, y,w, z));
+            }
+
+        }
+        int couuunter = 1;
+        for (Arista arista : aristaauxiliar) {
             g.setColor(Color.BLACK);
 
             Jugador nodo1 = arista.getJugador1();
             Jugador nodo2 = arista.getJugador2();
-            /*
-             * //g.drawLine(nodo1.x, nodo1.y, nodo2.x/2+ 50, nodo2.y/2 + 50);
-             * //g.drawLine(nodo2.x/2+ 50, nodo2.y/2 + 50, nodo2.x/2+ 50, nodo2.y/2 + 50);
-             * g.drawLine(nodo1.x, nodo1.y, nodo2.x, nodo2.y);
-             * for(Arista arista_new : campo.getAristas()){
-             *     if (arista_new.getJugador1.getNombre() == nodo2.getNombre() && arista_new.getJugador2.getNombre() == nodo1.getNombre()){
-             *         g.drawLine(nodo1.x, nodo1.y, nodo2.x/2+ 50, nodo2.y/2-50);
-             *     }
-             * }
-             * 
-             * 
-             * // Dibuja el valor de la arista
-             * g.setColor(Color.RED);
-             * int valorX = (nodo1.x + nodo2.x) / 2;
-             * int valorY = (nodo1.y + nodo2.y) / 2;
-             * g.drawString(String.valueOf(arista.getPeso()), valorX, valorY);
-             */
+            int x = nodo1.x;
+            int y = nodo1.y;
+            int w = nodo2.x;
+            int z = nodo2.y;
+            /*System.out.println("Arista:1");
+            System.out.println("x:" + x + " y:" + y);
+            System.out.println("w:" + w + " z:" + z);
+            System.out.println("arista:" + 2);
+            System.out.println("x:" + buscarArista(w, z,x, y).getJugador1().x + " y:" + buscarArista(w, z,x, y).getJugador1().y);
+            System.out.println("w:" + buscarArista(w, z,x, y).getJugador2().x + " z:" + buscarArista(w, z,x, y).getJugador2().y);*/
+            if (x == w && y == z) {
+                System.out.println("ARISTA:" + couuunter);
+                g.drawArc(x + 10, y + 5, 20, 30, 320, 290);
+                x = x + 15;
+                y = y + 20;
+                couuunter++;
+            } else {
+                if (x==w) {
+
+                    g.setColor(Color.BLACK);
+                    System.out.println("ARISTA:" + couuunter);
+                    g.drawLine(x, y, ((x - w) / 2) + w-10, ((y - z) / 2) + z - 10);
+                    g.drawLine(((x - w) / 2) + w-10, ((y - z) / 2) + z - 10, w, z);
+                    g.setColor(Color.decode("#3BCE1E"));
+                    g.drawString(String.valueOf(arista.getPeso()), ((x - w) / 2) + w-25, ((y - z) / 2) + z - 20);
+                    int x2 = w;
+                    int y2 = z;
+                    g.setColor(Color.BLACK);
+
+                    g.drawLine(x2, y2, ((x2 - x) / 2) + x+10, ((y2 - y) / 2) + y + 10);
+                    g.drawLine(((x2 - x) / 2) + x+10, ((y2 - y) / 2) + y + 10, x, y);
+
+                    g.setColor(Color.decode("#C256FC"));
+                    g.drawString(String.valueOf(buscarArista(w, z, x, y).getPeso()), ((x2 - x) / 2) + x+15, ((y2 - y) / 2) + y + 20);
+
+
+                    // h.drawLine(x + 18 + 20, y + 20 + 45, w + 20 + 18, z + 45 + 20);
+                    couuunter++;
+                } else {
+
+                g.setColor(Color.BLACK);
+                System.out.println("ARISTA:" + couuunter);
+                g.drawLine(x, y, ((x - w) / 2) + w, ((y - z) / 2) + z - 10);
+                g.drawLine(((x - w) / 2) + w, ((y - z) / 2) + z - 10, w, z);
+                g.setColor(Color.decode("#3BCE1E"));
+                g.drawString(String.valueOf(arista.getPeso()), ((x - w) / 2) + w, ((y - z) / 2) + z - 20);
+                int x2 = w;
+                int y2 = z;
+                g.setColor(Color.BLACK);
+                g.drawLine(x2, y2, ((x2 - x) / 2) + x, ((y2 - y) / 2) + y + 10);
+                g.drawLine(((x2 - x) / 2) + x, ((y2 - y) / 2) + y + 10, x, y);
+
+                    g.setColor(Color.decode("#C256FC"));
+                g.drawString(String.valueOf(buscarArista(w, z, x, y).getPeso()), ((x2 - x) / 2) + x, ((y2 - y) / 2) + y + 20);
+
+
+                // h.drawLine(x + 18 + 20, y + 20 + 45, w + 20 + 18, z + 45 + 20);
+                couuunter++;
+            }
+            }
+
+        }
+        // Dibuja las aristas y sus valores
+        /*for (Arista arista : campo.getAristas()) {
+            g.setColor(Color.BLACK);
+
+            Jugador nodo1 = arista.getJugador1();
+            Jugador nodo2 = arista.getJugador2();
+
 
             int x = nodo1.x;
             int y = nodo1.y;
@@ -187,8 +251,11 @@ public class ver extends javax.swing.JPanel {
                 x = x + 15;
                 y = y + 20;
             } else {
-           
-
+                e = x;
+                f = y;
+                //System.out.println("Jugador1:" + nodo1.getNombre());
+                //System.out.println("Jugador2:" + nodo2.getNombre());
+                //System.out.println("Valor peso:" + arista.getPeso());
                 // comprobar que 2 vertices no posean una arista
                 for (int i = 0; i < campo.getAristas().size(); i++) {
                     if (campo.getAristas().get(i).getJugador1().x == w && campo.getAristas().get(i).getJugador1().y == z
@@ -198,35 +265,24 @@ public class ver extends javax.swing.JPanel {
                     }
 
                 }
+                System.out.println("e:" + e);
                 if (e == 1) {
-                    int x2 = ((x + 20 - w) / 2) + w;
-                    int y2 = ((y + 45 - z + 45) / 2) + z;
-                    //g.drawLine(x, y, ((x - w) / 2) + w, ((y - z) / 2) + z - 10);
-                    g.drawString(String.valueOf(arista.getPeso()), x2, y2 - 40);
-                    g.setColor(Color.decode("#C256FC"));
+                    System.out.println(" entro a 1");
+                    g.drawLine(x, y, ((x - w) / 2) + w, ((y - z) / 2) + z - 10);
                     g.drawLine(((x + 20 - w) / 2) + w, ((y + 45 - z) / 2) + z - 10, w, z);
-                    
-                    
-                    System.out.println("Entro al 1");
-                   
-                    e=0;
-                } else {
-                    System.out.println("Entro al 2");
-
-                    g.drawLine(x, y, ((x - w) / 2) + w, ((y - z) / 2) + z + 30);
-                    g.drawLine(((x - w) / 2) + w, ((y - z) / 2) + z + 30, w, z);
                     int x2 = ((x + 20 - w) / 2) + w;
-                    int y2 = ((y + 45 - z + 45) / 2) + z;
-                    
-                    System.out.println("Entro al 2");
-                    g.drawString(String.valueOf(arista.getPeso()), x2, y2 + 40);
-                   // g.drawString(String.valueOf(arista.getPeso()), x2, y2 + 30);
-                }
-            }
+                    int y2 = ((y + 45 - z + 45) / 2) + z - 10;
+                    g.setColor(Color.decode("#3BCE1E"));
+                    g.drawString(String.valueOf(arista.getPeso()), x2, y2 - 40);
+               
+
+                // h.drawLine(x + 18 + 20, y + 20 + 45, w + 20 + 18, z + 45 + 20);
+
+            }}
 
             System.out.println("x:" + x1 + " y:" + y1);
             System.out.println("w:" + w + " z:" + z);
-        }
+        }*/
 
         // Dibuja los nodos y sus nombres
         for (Jugador nodo : campo.getJugadores()) {
